a648f073ecd2a8dbd88e4d58c434356660d94183
lichie <lichie567@protonmail.com>
2024-05-26 01:30:30 -0700

    update build script to replace toc versions

--- build.sh
+++ build.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 
 output="$1"
+version="$2"
 
-if [ -d './.release' ]; then
-  rm -rf './.release'
+if [ -d "./.release" ]; then
+  rm -rf "./.release"
 fi
 
-./packager/release.sh
+if [  ! -z "$version" -a "$version" != ""  ]; then
+  tocFiles=(
+    "ElvUI/ElvUI_Mainline.toc"
+    "ElvUI/ElvUI_Classic.toc"
+    "ElvUI/ElvUI_Cata.toc"
+    "ElvUI_Libraries/ElvUI_Libraries_Mainline.toc"
+    "ElvUI_Libraries/ElvUI_Libraries_Classic.toc"
+    "ElvUI_Libraries/ElvUI_Libraries_Cata.toc"
+    "ElvUI_Options/ElvUI_Options_Mainline.toc"
+    "ElvUI_Options/ElvUI_Options_Classic.toc"
+    "ElvUI_Options/ElvUI_Options_Cata.toc"
+  )
 
-cp ./.release/ElvUI-*.zip "$output"
+  for file in "${tocFiles[@]}"
+  do
+    sed -i "s/Version: .*$/Version: v$version/" "$file"
+  done
+
+  if [  ! -z "$output" -a "$output" != ""  ]; then
+    ./packager/release.sh
+    cp ./.release/ElvUI-*.zip "$output"
+  fi
+fi
